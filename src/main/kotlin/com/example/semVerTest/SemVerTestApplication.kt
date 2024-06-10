f7e3551eb330727023ec2115327bed4155b02467
Omar ElSafwany <omar.safwany@gmail.com>
2024-06-10 11:37:23 +0300

    first commit

---
+++ src/main/kotlin/com/example/semVerTest/SemVerTestApplication.kt
@@ -0,0 +1,11 @@
+package com.example.semVerTest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SemVerTestApplication
+
+fun main(args: Array<String>) {
+	runApplication<SemVerTestApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/semVerTest/SemVerTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.semVerTest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SemVerTestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
